35846828ddde4ea8cc65e2d2691e959edd72b732
Pascal Aondover <70010666+kayung-developer@users.noreply.github.com>
2025-09-24 13:01:10 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import { AppearanceProvider } from './context/AppearanceContext';
 import { NotificationProvider } from './context/NotificationContext';
 
