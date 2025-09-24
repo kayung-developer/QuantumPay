9d39ee647d64ed6f1ca6cd01f1c6167fbb240ee6
Pascal Aondover <70010666+kayung-developer@users.noreply.github.com>
2025-09-24 19:12:07 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
 import { AppearanceProvider } from './context/AppearanceContext';
 
 
@@ -25,33 +24,6 @@ root.render(
       <AuthProvider>
       <AppearanceProvider>
         <App />
-        <Toaster
-          position="top-right"
-          reverseOrder={false}
-          toastOptions={{
-            className: '',
-            duration: 5000,
-            style: {
-              background: '#1E293B', // neutral-800
-              color: '#F1F5F9', // neutral-100
-              border: '1px solid #334155', // neutral-700
-            },
-            success: {
-              duration: 3000,
-              theme: {
-                primary: '#059669', // secondary
-                secondary: '#F1F5F9',
-              },
-            },
-            error: {
-              duration: 5000,
-              theme: {
-                primary: '#DC2626', // red-600
-                secondary: '#F1F5F9',
-              },
-            },
-          }}
-        />
         </AppearanceProvider>
       </AuthProvider>
     </BrowserRouter>
