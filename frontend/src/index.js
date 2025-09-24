3c6690a1d313d46b1a41e9f9f6d28257e4a6b9a4
Pascal Aondover <70010666+kayung-developer@users.noreply.github.com>
2025-09-24 21:45:40 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,6 +24,7 @@ root.render(
       <AuthProvider>
       <AppearanceProvider>
         <App />
+        <CustomToaster />
         </AppearanceProvider>
       </AuthProvider>
     </BrowserRouter>
