19c157bda6c23df40105e03325d58eaee4c5fb50
Pascal Aondover <70010666+kayung-developer@users.noreply.github.com>
2025-09-25 06:44:21 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,6 @@ import { CheckCircleIcon, XCircleIcon, InformationCircleIcon, ExclamationTriangl
 // --- Context Providers ---
 import { AuthProvider } from './context/AuthContext';
 import { AppearanceProvider, useAppearance } from './context/AppearanceContext'; // <-- Import useAppearance
-import { Transition } from '@headlessui/react';
 import { CheckCircleIcon, XCircleIcon, InformationCircleIcon, ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 
 // --- Other Imports ---
